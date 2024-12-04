401e9d58e7bf4e45cc649e7de6e1683c5e6ec312
moka-o <moka.o@kakaocorp.com>
2024-12-04 18:13:55 +0900

    🧑‍💻 children props 인터페이스 확장

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-function createElement(type, props, ...children) {
-  console.log('createElement', type, props.className)
+/** @jsx createElement */
+
+function createElement(type, props, ..._children) {
+  const propsChildren =
+    typeof props.children === 'string' || !isIterable(props.children)
+      ? [props.children].filter(Boolean)
+      : props.children
+
+  const children = [..._children, ...propsChildren]
+
   return {
     type,
     props: {
@@ -19,13 +27,44 @@ function createTextElement(text) {
   }
 }
 
-/** @jsx createElement */
-const element = (
+const elem1 = (
   <div className="1">
     <div className="3">
-      <div className="2">hello</div>
+      <div className="2">
+        {'hello'}
+        {'hello'}
+      </div>
     </div>
   </div>
 )
 
-console.log(element)
+const elem2 = <div className="1" children={<div className="3" children={<div className="2" children={'hello'} />} />} />
+
+const elem3 = createElement(
+  'div',
+  {className: '1'},
+  createElement('div', {className: '3'}, createElement('div', {className: '2'}, 'hello'))
+)
+
+const elem4 = <div className="2" children="hello" />
+
+render(elem1)
+
+function render(vdom, deps = 0) {
+  delete vdom.props.__self
+  delete vdom.props.__source
+
+  console.log(' '.repeat(deps), vdom.props.nodeValue || `${vdom.type} ${vdom.props.className}`)
+
+  vdom.props.children.forEach((child) => {
+    render(child, deps + 1)
+  })
+}
+
+function isIterable(obj) {
+  if (!obj) {
+    return false
+  }
+
+  return typeof obj[Symbol.iterator] === 'function'
+}
