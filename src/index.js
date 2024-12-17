e24279cfd8b7356ba2e8f4777ac9c0ccbb8b0720
moka-o <moka.o@kakaocorp.com>
2024-12-17 16:09:49 +0900

    ✨ reconcile

--- src/index.js
+++ src/index.js
@@ -81,16 +81,30 @@ function createDom(vdom) {
 }
 
 function render(element, container) {
+  deletions = []
   nextUnitOfWork = wipRoot = {
     dom: container,
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   }
 }
 
+function isEvent(key) {
+  return key.startsWith('on')
+}
+
 function isProperty(key) {
-  return key !== 'children'
+  return key !== 'children' && !isEvent(key)
+}
+
+function isNew(prev, next) {
+  return (key) => prev[key] !== next[key]
+}
+
+function isGone(prev, next) {
+  return (key) => !(key in next)
 }
 
 function isIterable(obj) {
@@ -104,7 +118,9 @@ function isIterable(obj) {
 const root = document.getElementById('root')
 
 let nextUnitOfWork = null
+let currentRoot = null
 let wipRoot = null
+let deletions = null
 
 ;(function workLoop(deadline) {
   let shouldYield = false
@@ -123,8 +139,50 @@ let wipRoot = null
   console.log('check')
 })()
 
+function appendDom(parentDom, dom) {
+  parentDom.appendChild(dom)
+}
+
+function removeDom(parentDom, dom) {
+  parentDom.removeChild(dom)
+}
+
+function updateDom(dom, prevProps, nextProps) {
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.removeEventListener(eventType, prevProps[name])
+    })
+
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = ''
+    })
+
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name]
+    })
+
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, nextProps[name])
+    })
+}
+
 function commitRoot() {
+  deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  currentRoot = wipRoot
   wipRoot = null
 }
 
@@ -134,7 +192,14 @@ function commitWork(fiber) {
   }
 
   const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
+
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom) {
+    appendDom(domParent, fiber.dom)
+  } else if (fiber.effectTag === 'DELETION') {
+    removeDom(domParent, fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+  }
 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
@@ -162,14 +227,41 @@ function performUnitOfWork(fiber) {
 }
 
 function reconcileChildren(wipFiber, elements) {
+  let oldFiber = wipFiber.alternate?.child
   let prevSibling = null
 
-  elements.forEach((element, i) => {
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: wipFiber,
-      dom: null,
+  for (let i = 0; i < elements.length || oldFiber; i++) {
+    const element = elements[i]
+
+    let newFiber = null
+
+    const isSameType = element && oldFiber && element.type === oldFiber.type
+
+    if (isSameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: 'UPDATE',
+      }
+    } else {
+      if (element) {
+        newFiber = {
+          type: element.type,
+          props: element.props,
+          parent: wipFiber,
+          dom: null,
+          alternate: null,
+          effectTag: 'PLACEMENT',
+        }
+      }
+
+      if (oldFiber) {
+        oldFiber.effectTag = 'DELETION'
+        deletions.push(oldFiber)
+      }
     }
 
     if (i === 0) {
@@ -179,7 +271,7 @@ function reconcileChildren(wipFiber, elements) {
     }
 
     prevSibling = newFiber
-  })
+  }
 }
 
 render(elem1, root)
